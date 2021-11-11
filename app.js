8e0e6fb37fb749516209f8fa9c6d85bbb364cd07
Dan Kercher <dankercher@gmail.com>
2021-11-11 11:28:34 -0500

    basic ts types

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Test test test');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5; // not best practice to write number1: number = 5 Inferred at initialization
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is ';
+add(number1, number2, printResult, resultPhrase);
