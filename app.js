04f5594dc322f5d47dab5d3164ef1981f82fd6c5
Dan Kercher <dankercher@gmail.com>
2021-11-15 09:07:54 -0500

    objects

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5; // not best practice to write number1: number = 5 Inferred at initialization
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is ';
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'Dan',
+    age: 32
+};
+console.log(person.nickname);
--- app.ts
+++ app.ts
@@ -1,15 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string){
-    const result = n1 + n2;
-    if(showResult){
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
-}
+const person = {
+    name: 'Dan',
+    age: 32
+};
 
-const number1 = 5;  // not best practice to write number1: number = 5 Inferred at initialization
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.nickname);
